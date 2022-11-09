31b303f53ceb29046656f897b8572e3c64197e19
iimagii <mag_1@ukr.net>
2022-11-10 01:57:18 +0200

    new deploy

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 app.use(express.static(__dirname))
-app.use(express.static(path.resolve(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
